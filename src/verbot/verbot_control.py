21a3169bdabc5cb10c32f15a92a8f9514a5d68e8
Neil Davis <neil.a.davis@gmail.com>
2021-02-16 23:37:52 +0000

    Use typing and properties

--- src/verbot/verbot_control.py
+++ src/verbot/verbot_control.py
@@ -1,60 +1,66 @@
 from enum import Enum
+from abc import ABC
 import RPi.GPIO as GPIO
+from typing import *
 
-class VerbotController():
+class State(Enum):
+    """Valid states of Controller instances"""
+    INTERROGATE     = 0
+    STOP            = 1
+    ROTATE_RIGHT    = 2
+    ROTATE_LEFT     = 3
+    FORWARDS        = 4
+    REVERSE         = 5
+    PICK_UP         = 6
+    PUT_DOWN        = 7
+    TALK            = 8
+
+class Controller():
     """GPIO controller for Verbot"""
 
-    class State(Enum):
-        """Valid states of VerbotController instances"""
-        INTERROGATE     = 0
-        STOP            = 1
-        ROTATE_RIGHT    = 2
-        ROTATE_LEFT     = 3
-        FORWARDS        = 4
-        REVERSE         = 5
-        PICK_UP         = 6
-        PUT_DOWN        = 7
-        TALK            = 8
-
-    class Observer:
-        """Defines the interface for state observers of VerbotController instances"""
-        def state_will_change(self, state):
+    class Observer(ABC):
+        """Abstract - Defines the interface for state observers of Controller instances"""
+        def state_will_change(self, state: State) -> None:
             """Called when the a new desired state is requested"""
             pass
-        def state_did_change(self, state):
+
+        def state_did_change(self, state: State) -> None:
             """Called when the new desired state is reached"""
             pass
 
 
-    @property
-    def current_state(self):
-        return self.__current_state
-
     def __init__(self):
         """c'tor"""
-        self.__current_state = self.State.STOP
-        self.__desired_state = self.State.STOP
+        self.__current_state = State.STOP
+        self.__desired_state = State.STOP
         self.__observers = []
 
-    def add_observer(self, obs):
+    @property
+    def current_state(self) -> State:
+        """Returns the current state"""
+        return self.__current_state
+
+    @property 
+    def desired_state(self) -> State:
+        """Returns the desired state"""
+        return self.__desired_state
+
+    @desired_state.setter
+    def desired_state(self, state: State) -> None:
+       """Request a new desired state"""
+       self.__desired_state = state
+       self._notify_new_desired_state()
+
+    def add_observer(self, obs: Observer) -> None:
         """Add an object as an observer to receive state chnage notifications. 
-        The observer object should implement the methods in the VerbotController.Observer class
+        The observer object should implement the methods in the Controller.Observer class
         """
         if obs not in self.__observers:
             self.__observers.append(obs)
     
-    def remove_observer(self, obs):
+    def remove_observer(self, obs: Observer) -> None:
         """Remove an observer object"""
         self.__observers.remove(obs)
-
-    def move_forward(self):
-        """Request for Verbot to move forwards"""
-        self._set_new_desired_state(self.State.FORWARDS)
-
-
-    def _set_new_desired_state(self, state):
-       self.__desired_state = state
-       self._notify_new_desired_state()
 
     def _set_new_current_state(self, state):
        self.__current_state = state
--- src/verbot_pi.py
+++ src/verbot_pi.py
@@ -1,9 +1,24 @@
-from verbot.verbot_control import VerbotController as Verbot
+from verbot.verbot_control import State as VerbotState, Controller as Verbot
+import RPi.GPIO as GPIO
+
+class VerbotObserver(Verbot.Observer):
+    def state_will_change(self, state: VerbotState) -> None:
+        print("New desired state={0:s}\n".format(state.name))
+        pass
+    
+    def state_did_change(self, state: VerbotState) -> None:
+        print("New current state={0:s}\n".format(state.name))
+        pass
 
 def main():
-    vb = Verbot()
+    try:
+        vbo = VerbotObserver()
+        vb = Verbot()
+        vb.add_observer(vbo)
+        vb.desired_state = VerbotState.TALK
+    finally:
+        GPIO.cleanup()
 
 if __name__ == "__main__":
     # execute only if run as a script
     main()
-    
